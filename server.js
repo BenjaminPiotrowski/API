3c1e840cc0cd923b4a5c2f0364008b4de3ca6a37
Benjamin Piotrowski <benjamin_piotrowski@yahoo.com>
2022-02-13 17:43:55 -0500

    adding CSS, visualizations and User interactions

--- server.js
+++ server.js
@@ -10,238 +10,165 @@ server.get('/', (req, res) => {
   renderToString(app).then((html) => {
     res.send(`
     <!DOCTYPE html>
-    <html>
-    <head>
-        <title></title>
-        <meta charset="utf-8" />
-        <script type='text/javascript'>
-        var map, searchManager;
+<html>
+<head>
+    <title>Map API</title>
+    <meta charset="utf-8" />
+    <script type='text/javascript'>
+    var map, searchManager;
+
+    function GetMap() {
+        map = new Microsoft.Maps.Map('#myMap', {
+            credentials: 'ApmKoIWURuxK3tCczUtprA-nEzffTQpnCKRup0ZpsvNxFDi8Q7ivacUiA2gVRCBG'
+        });
+        addCode();
+    }
+    function addCode() {
+      var link = document.createElement('link'); 
+  
+      // set the attributes for link element
+         link.rel = 'stylesheet'; 
     
-        function GetMap() {
-            map = new Microsoft.Maps.Map('#myMap', {
-                credentials: 'ApmKoIWURuxK3tCczUtprA-nEzffTQpnCKRup0ZpsvNxFDi8Q7ivacUiA2gVRCBG'
+      link.type = 'text/css';
+    
+      link.href = 'button.css'; 
+
+      // Get HTML head element to append 
+      // link element to it 
+      document.getElementsByTagName('HEAD')[0].appendChild(link); 
+
+      // Create new button Element
+        var button = document.createElement('button'); 
+  
+        button.innerText = 'Where am I?'
+        // Attach the "click" event to your button
+        button.addEventListener('click', () => {
+          reverseGeocode()
+        })      
+        button.style = 'left:250px;height:100px;top: 430px; position: relative; border-radius: 50%; background-color:rgba(0,255,203,.64);'; 
+      document.getElementById("myMap").appendChild(button);
+    }
+    function Search() {
+        if (!searchManager) {
+            //Create an instance of the search manager and perform the search.
+            Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+                searchManager = new Microsoft.Maps.Search.SearchManager(map);
+                Search()
             });
+        } else {
+            //Remove any previous results from the map.
+            map.entities.clear();
+
+            //Get the users query and geocode it.
+            var query = document.getElementById('searchTbx').value;
+            geocodeQuery(query);
         }
-    
-        function Search() {
-            if (!searchManager) {
-                //Create an instance of the search manager and perform the search.
-                Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
-                    searchManager = new Microsoft.Maps.Search.SearchManager(map);
-                    Search()
-                });
-            } else {
-                //Remove any previous results from the map.
-                map.entities.clear();
-    
-                //Get the users query and geocode it.
-                var query = document.getElementById('searchTbx').value;
-                geocodeQuery(query);
-            }
-        }
-    
-        function geocodeQuery(query) {
-            var searchRequest = {
-                where: query,
-                callback: function (r) {
-                    if (r && r.results && r.results.length > 0) {
-                        var pin, pins = [], locs = [], output = 'Results:<br/>';
-    
-                        for (var i = 0; i < r.results.length; i++) {
-                            //Create a pushpin for each result. 
-                            pin = new Microsoft.Maps.Pushpin(r.results[i].location, {
-                                text: i + ''
-                            });
-                            pins.push(pin);
-                            locs.push(r.results[i].location);
-    
-                            output += i + ') ' + r.results[i].name + '<br/>';
-                        }
-    
-                        //Add the pins to the map
-                        map.entities.push(pins);
-    
-                        //Display list of results
-                        document.getElementById('output').innerHTML = output;
-    
-                        //Determine a bounding box to best view the results.
-                        var bounds;
-    
-                        if (r.results.length == 1) {
-                            bounds = r.results[0].bestView;
-                        } else {
-                            //Use the locations from the results to calculate a bounding box.
-                            bounds = Microsoft.Maps.LocationRect.fromLocations(locs);
-                        }
-    
-                        map.setView({ bounds: bounds });
+    }
+
+    function geocodeQuery(query) {
+        var searchRequest = {
+            where: query,
+            callback: function (r) {
+                if (r && r.results && r.results.length > 0) {
+                    var pin, pins = [], locs = [], output = 'Results:<br/>';
+
+                    for (var i = 0; i < r.results.length; i++) {
+                        //Create a pushpin for each result. 
+                        pin = new Microsoft.Maps.Pushpin(r.results[i].location, {
+                            text: i + ''
+                        });
+                        pins.push(pin);
+                        locs.push(r.results[i].location);
+
+                        output += i + ') ' + r.results[i].name + '<br/>';
                     }
-                },
-                errorCallback: function (e) {
-                    //If there is an error, alert the user about it.
-                    alert("No results found.");
+
+                    //Add the pins to the map
+                    map.entities.push(pins);
+
+                    //Display list of results
+                    document.getElementById('output').innerHTML = output;
+
+                    //Determine a bounding box to best view the results.
+                    var bounds;
+
+                    if (r.results.length == 1) {
+                        bounds = r.results[0].bestView;
+                    } else {
+                        //Use the locations from the results to calculate a bounding box.
+                        bounds = Microsoft.Maps.LocationRect.fromLocations(locs);
+                    }
+
+                    map.setView({ bounds: bounds });
                 }
-            };
+            },
+            errorCallback: function (e) {
+                //If there is an error, alert the user about it.
+                alert("No results found.");
+            }
+        };
+
+        //Make the geocode request.
+        searchManager.geocode(searchRequest);
+    }
+    function reverseGeocode() {
+      //If search manager is not defined, load the search module.
+      if (!searchManager) {
+          //Create an instance of the search manager and call the reverseGeocode function again.
+          Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+              searchManager = new Microsoft.Maps.Search.SearchManager(map);
+              reverseGeocode();
+          });
+      } else {
+          var searchRequest = {
+              location: map.getCenter(),
+              callback: function (r) {
+                  //Tell the user the name of the result.
+                  document.getElementById("titletext").innerHTML = r.name;
+                  //alert(r.name);
+              },
+              errorCallback: function (e) {
+                  //If there is an error, alert the user about it.
+                  alert("Unable to reverse geocode location.");
+              }
+          };
+
+          //Make the reverse geocode request.
+          searchManager.reverseGeocode(searchRequest);
+      }
+  }
+    </script>
+    <script type='text/javascript' src='http://www.bing.com/api/maps/mapcontrol?callback=GetMap' async defer></script>
+</head>
+<body style="background-image: url('earth_panorama_4k.jpg');">
+<center><h1 id='titletext' style='color:rgba(0,255,203,.64); '></h1></center>
+<div alight="center" style="position:fixed;width:600px;height:400px;top:50%;left:50%">
+    <div style="position:relative;top:-210px;left:-300px">
+     <input id='searchTbx' style='width:529px' type='text'/>
+     <input type='button'value='Search' onclick='Search()'/>
     
-            //Make the geocode request.
-            searchManager.geocode(searchRequest);
-        }
-        </script>
-        <script type='text/javascript' src='http://www.bing.com/api/maps/mapcontrol?callback=GetMap' async defer></script>
-    </head>
-    <body>
-        <input id='searchTbx' type='text'/>
-        <input type='button'value='Search' onclick='Search()'/>
-        <br/>
-        <div id="myMap" style="position:relative;width:600px;height:400px;float:left;"></div>
-        <div id='output' style="margin-left:10px;float:left;"></div>
-    </body>
-    </html>
+    <br/>
+    
+      <div id="myMap" style="position:relative;width:600px;height:400px;float:left;">
+        
+      </div>
+      <div id='output' style="position:relative;background-color:white;margin-left:0px;float:left;height:400px;left:600px;top:-400px"></div>
+    </div>
+    </div>
+</body>
+</html>
     `);
   });
 });
 
 server.use(express.static('.'));
 
+//output to console to notify when the page is ready to be tested.
 server.listen(3000, () => {
-  console.log('ready');
+  console.log('Web Page is Up');
 });
 
-
-//<!DOCTYPE html>
-//    <html>
-//      <head>
-//        <title>Vue SSR Example</title>
-//        <script type="importmap">
-//          {
-//            "imports": {
-//              "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
-//            }
-//          }
-//        </script>
-//        <script type="module" src="/client.js"></script>
-//      </head>
-//      <body>
-//        <div id="app">${html}</div>
-//      </body>
-//    </html>
-
-/*< !DOCTYPE html >
-  <html>
-    <head>
-      <title></title>
-      <meta charset="utf-8" />
-      <script type='text/javascript'>
-        var map, baseLayer, tools, currentShape;
-
-        function GetMap() {
-          map = new Microsoft.Maps.Map('#myMap', {
-            credentials: 'ApmKoIWURuxK3tCczUtprA-nEzffTQpnCKRup0ZpsvNxFDi8Q7ivacUiA2gVRCBG'
-          });
-
-        //Create a base layer to add drawn shapes.
-        baseLayer = new Microsoft.Maps.Layer();
-        map.layers.insert(baseLayer);
-
-        //Load the DrawingTools module.
-        Microsoft.Maps.loadModule('Microsoft.Maps.DrawingTools', function () {
-          //Create an instance of the DrawingTools class and bind it to the map.
-          tools = new Microsoft.Maps.DrawingTools(map);
-
-        Microsoft.Maps.Events.addHandler(tools, 'drawingChanging', function (e) {
-          //When the drawing is changing, clear the base layer.
-          baseLayer.clear();
-            });
+//<input id="clickme" style="position:relative;top:400px;" type="button" value="Where Am I?" onclick="reverseGeocode();" />
 
 
-        //When the user presses 'esc', take the polygon out of edit mode and re-add to base map.
-        document.getElementById('myMap').onkeypress = function (e) {
-                if (e.charCode === 27) {
-          tools.finish(shapeDrawn);
-        currentShape = null;
-                }
-            };
-        });
-    }
 
-        function drawPolygon() {
-        //Stop any current drawing.
-        if (currentShape) {
-          tools.finish(shapeDrawn);
-        currentShape = null;
-        }
-
-        //Create a new polygon.
-        tools.create(Microsoft.Maps.DrawingTools.ShapeType.polygon, function (s) {
-          currentShape = s;
-        });
-    }
-
-        function shapeDrawn(s) {
-          baseLayer.add(s);
-    }
-      </script>
-      <script type='text/javascript' src='https://www.bing.com/api/maps/mapcontrol?callback=GetMap' async defer></script>
-    </head>
-    <body>
-      <div id="myMap" style="position:relative;width:600px;height:400px;"></div><br />
-      <input type="button" onclick="drawPolygon()" value="Draw Polygon" />
-    </body>
-  </html> */
-
- /* <!DOCTYPE html>
-  <html>
-  <head>
-      <title></title>
-      <meta charset="utf-8" />
-      <script type='text/javascript'>
-      var map, searchManager;
-  
-      function GetMap() {
-          map = new Microsoft.Maps.Map('#myMap', {
-              credentials: 'ApmKoIWURuxK3tCczUtprA-nEzffTQpnCKRup0ZpsvNxFDi8Q7ivacUiA2gVRCBG'
-          });
-  
-          //Make a request to geocode New York, NY.
-          geocodeQuery("New York, NY");
-      }
-  
-      function geocodeQuery(query) {
-          //If search manager is not defined, load the search module.
-          if (!searchManager) {
-              //Create an instance of the search manager and call the geocodeQuery function again.
-              Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
-                  searchManager = new Microsoft.Maps.Search.SearchManager(map);
-                  geocodeQuery(query);
-              });
-          } else {
-              var searchRequest = {
-                  where: query,
-                  callback: function (r) {
-                      //Add the first result to the map and zoom into it.
-                      if (r && r.results && r.results.length > 0) {
-                          var pin = new Microsoft.Maps.Pushpin(r.results[0].location);
-                          map.entities.push(pin);
-  
-                          map.setView({ bounds: r.results[0].bestView });
-                      }
-                  },
-                  errorCallback: function (e) {
-                      //If there is an error, alert the user about it.
-                      alert("No results found.");
-                  }
-              };
-  
-              //Make the geocode request.
-              searchManager.geocode(searchRequest);
-          }
-      }
-      </script>
-      <script type='text/javascript' src='http://www.bing.com/api/maps/mapcontrol?callback=GetMap' async defer></script>
-  </head>
-  <body>
-      <div id="myMap" style="position:relative;width:600px;height:400px;"></div>
-  </body>
-  </html>
-
-  */
